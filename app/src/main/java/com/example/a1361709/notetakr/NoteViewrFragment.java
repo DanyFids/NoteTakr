75b3a20a242cbbf89b893eb39561729b35eadf79
DanyFids <1361709@p322-19.studentservers.jac-servers.johnabbott.qc.ca>
2016-09-22 13:33:49 -0400

    As2Start

---
+++ app/src/main/java/com/example/a1361709/notetakr/NoteViewrFragment.java
@@ -0,0 +1,22 @@
+package com.example.a1361709.notetakr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NoteViewrFragment extends Fragment {
+
+    public NoteViewrFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_viewr, container, false);
+    }
+}
